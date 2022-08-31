fbe763b0ad379755e20ac49a8642e94bca646380
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-08-31 23:47:35 +0700

    I'm not sure if this gonna works but hey! lets try

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,43 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  let min, mid, max
+  if (n1 > n2) {
+    max = n1
+    if (n2 > n3) {
+      mid = n2
+      min = n3
+    }
+    else {
+      mid = n3
+      min = n2
+    }
+  }
+  else if (n2 > n3) {
+    max = n2
+    if (n1 > n3) {
+      mid = n1
+      min = n3
+    }
+    else {
+      mid = n3
+      min = n1
+    }
+  }
+  else {
+    max = n3
+    if (n1 > n2) {
+      mid = n1
+      min = n2
+    }
+    else {
+      mid = n2
+      min = n1
+    }
+  }
+  console.log({ min, mid, max })
 }
+
+minMedMax(-1, -8, 0)
+
 module.exports = minMedMax
